2fd62aa9fba225395b2bb88dc4d58f145e444bda
Pulkit verma <pulkit18012003@gmail.com>
2025-04-05 18:25:04 +0530

    Initial commit

---
+++ app/src/main/java/in/kay/furture/screens/CheckoutScreen.kt
@@ -0,0 +1,11 @@
+package `in`.kay.furture.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun CheckoutScreen(navController: NavHostController) {
+    // Your checkout screen UI here
+    Text("This is Checkout Page")
+}
